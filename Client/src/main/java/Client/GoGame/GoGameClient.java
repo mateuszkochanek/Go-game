eac12e53fa962c99d65203a6ea7babc51a7011f4
Erthax <mateuszkochanek01@gmail.com>
2019-11-28 20:42:50 +0100

    Create basic connection

--- Client/src/main/java/Client/GoGame/GoGameClient.java
+++ Client/src/main/java/Client/GoGame/GoGameClient.java
@@ -1,13 +1,27 @@
 package Client.GoGame;
 
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 /**
  * Hello world!
  *
  */
 public class GoGameClient 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String[] args) throws Exception {
+		String ip = "localhost";
+        try (var socket = new Socket(ip, 59898)) {
+            System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
+            var scanner = new Scanner(System.in);
+            var in = new Scanner(socket.getInputStream());
+            var out = new PrintWriter(socket.getOutputStream(), true);
+            while (scanner.hasNextLine()) {
+                out.println(scanner.nextLine());
+                System.out.println(in.nextLine());
+            }
+        }
     }
 }
