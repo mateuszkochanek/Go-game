3f5e98d478c622da06ae5222b951e0e631860f5d
Erthax <mateuszkochanek01@gmail.com>
2019-12-01 15:29:38 +0100

    Add command abstract class extensions

---
+++ Client/src/main/java/ClientApplication/GoGame/Entities/Commands/MoveCommand.java
@@ -0,0 +1,11 @@
+package ClientApplication.GoGame.Entities.Commands;
+
+public class MoveCommand extends Command {
+
+	@Override
+	protected void executeCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- Client/src/main/java/ClientApplication/GoGame/Entities/Game/Game.java
+++
@@ -1,5 +0,0 @@
-package ClientApplication.GoGame.Entities.Game;
-
-public class Game {
-
-}
