602dafc9dbbb13680ac8529e893f7aaa107c194c
Paweł Data <123pawel99@gmail.com>
2019-12-01 15:40:18 +0100

    Add command factories

---
+++ Client/src/main/java/ClientApplication/GoGame/Command/Factory/ConcreteCommandFactory.java
@@ -0,0 +1,22 @@
+package ClientApplication.GoGame.Command.Factory;
+
+import Server.ServerMessage.EndGame;
+import Server.ServerMessage.GameSettings;
+import Server.ServerMessage.MoveInfo;
+import Server.ServerMessage.OponentMove;
+import Server.ServerMessage.ServerMessage;
+
+public class ConcreteCommandFactory implements CommandFactory {
+    //TODO import command package and uncomment
+    /*public Command getCommand(ServerMessage message) {
+        if (message instanceof GameSettings) {
+            return new GameSettingsCommand();
+        } else if (message instanceof MoveInfo) {
+            return new MoveInfoCommand();
+        } else if (message instanceof OponentMove) {
+            return new OponentMoveCommand();
+        } else if (message instanceof EndGame) {
+            return new EndGameCommand();
+        }
+    }*/
+}
