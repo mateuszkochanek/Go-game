0fbabfb2870d4ff2aa4f4912b6351be83a3489dd
Paweł Data <123pawel99@gmail.com>
2019-12-12 18:15:48 +0100

    Change move command

--- Server/src/main/java/Server/ServerMessage/MoveInfo.java
+++ Server/src/main/java/Server/ServerMessage/MoveInfo.java
@@ -3,13 +3,32 @@ package Server.ServerMessage;
 public class MoveInfo implements ServerMessage {
     
     private static final long serialVersionUID = 1L;
+    private int player;
+    private int x;
+    private int y;
     int[][] emptyPlaces;
 	boolean correctMove;
+    
 
-	public MoveInfo(boolean correctMove, int[][] emptyPlaces) {
+	public MoveInfo(int player, boolean correctMove, int x, int y, int[][] emptyPlaces) {
+	    this.player = player;
+	    this.x = x;
+	    this.y = y;
 		this.correctMove = correctMove;
 		this.emptyPlaces = emptyPlaces;
 	}
+	
+	public int getPlayer() {
+	    return this.player;
+	}
+	
+	public int getX() {
+	    return this.x;
+	}
+	
+	public int getY() {
+        return this.y;
+    }
 	
 	public int[][] getEmptyPlaces() {
 	    return this.emptyPlaces;
