878d268937d2aa7c1bcae3c90243cf3807f0ef57
Erthax <mateuszkochanek01@gmail.com>
2019-12-15 16:36:49 +0100

    Refactor commands to work with new frames

--- Client/src/main/java/ClientApplication/GoGame/Entities/Commands/MoveCommand.java
+++ Client/src/main/java/ClientApplication/GoGame/Entities/Commands/MoveCommand.java
@@ -7,6 +7,7 @@ import ClientApplication.GoGame.Entities.ClientMessages.ClientMessage;
 import ClientApplication.GoGame.Entities.ClientMessages.Move;
 import ClientApplication.GoGame.Entities.ClientMessages.Pass;
 import ClientApplication.GoGame.Gui.GameGui;
+import ClientApplication.GoGame.Gui.Controller.GameBoardController;
 import Server.ServerMessage.MoveInfo;
 import Server.ServerMessage.ServerMessage;
 
@@ -23,8 +24,10 @@ public class MoveCommand extends Command {
 		int y = ((MoveInfo) serverMessage).getY();
 		int[][] empty = ((MoveInfo) serverMessage).getEmptyPlaces();
 		int color = ((MoveInfo) serverMessage).getPlayer();
+		
 		if(doMove) {
-			gameGui.doMove(x,y,empty,color);
+			((GameBoardController)gameGui.getFrame().getController()).showMove(x, y, color);
+			((GameBoardController)gameGui.getFrame().getController()).emptyPlaces(empty);;
 		} else {
 			//TODO what to do when move is not correct?
 		}
--- Client/src/main/java/ClientApplication/GoGame/Entities/Commands/SetGameSettingsCommand.java
+++ Client/src/main/java/ClientApplication/GoGame/Entities/Commands/SetGameSettingsCommand.java
@@ -1,6 +1,8 @@
 package ClientApplication.GoGame.Entities.Commands;
 
 import ClientApplication.GoGame.Gui.GameGui;
+import ClientApplication.GoGame.Gui.Frame.GameBoardFrame;
+import ClientApplication.GoGame.Gui.Frame.NewGameFrame;
 import Server.ServerMessage.SentGameOptions;
 import Server.ServerMessage.ServerMessage;
 
@@ -12,7 +14,11 @@ public class SetGameSettingsCommand extends Command {
 
     @Override
     public void executeCommand() {
-    	gameGui.CreateGameBoardFrame(((SentGameOptions)serverMessage).getSize());
+    	int size = ((SentGameOptions)serverMessage).getSize();
+        gameGui.setFrame(new GameBoardFrame(gameGui, size));
+        gameGui.getStage().close();
+        gameGui.setStage(gameGui.getFrame().getStage());
+        gameGui.getStage().show();
 	}
 
 
