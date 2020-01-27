10f1abf358d3786c89a05d5207c5df1a9625dd49
Paweł Data <User@LAPTOP-UQA391IV.home>
2020-01-27 10:34:14 +0100

    Add new messages

--- Server/src/main/java/ClientApplication/GoGame/Entities/ClientMessages/SetGameOptions.java
+++ Server/src/main/java/ClientApplication/GoGame/Entities/ClientMessages/SetGameOptions.java
@@ -5,10 +5,12 @@ public class SetGameOptions implements ClientMessage{
 	private static final long serialVersionUID = 1L;
 	int size;
 	String mode; //hotSeat, Online, Multiplayer
+	int gameId;
 	
-	public SetGameOptions(int size, String mode) {
+	public SetGameOptions(int size, String mode, int gameId) {
 		this.size = size;
 		this.mode = mode;
+		this.gameId = gameId;
 	}
 	public int getSize() {
 		return size;
@@ -22,6 +24,10 @@ public class SetGameOptions implements ClientMessage{
 	public void setMode(String mode) {
 		this.mode = mode;
 	}
-	
-	
+  public int getGameId() {
+    return gameId;
+  }
+  public void setGameId(int gameId) {
+    this.gameId = gameId;
+  }
 }
